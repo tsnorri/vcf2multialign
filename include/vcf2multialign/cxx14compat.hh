0d2d6f478369eaefc36a24db0fda4154a0bfe72d
Tuukka Norri <tsnorri@iki.fi>
2017-03-05 16:36:54 +0200

    Implement std::equal with four parameters for C++11

---
+++ include/vcf2multialign/cxx14compat.hh
@@ -0,0 +1,29 @@
+/*
+ Copyright (c) 2017 Tuukka Norri
+ This code is licensed under MIT license (see LICENSE for details).
+*/
+
+#ifndef VCF2MULTIALIGN_CXX14COMPAT_HH
+#define VCF2MULTIALIGN_CXX14COMPAT_HH
+
+#if __cplusplus < 201402L
+namespace std {
+	template <typename t_input_it_1, typename t_input_it_2>
+	bool equal(t_input_it it_1, t_input_it const end_1, t_input_it it_2, t_input_it const end_2)
+	{
+		while (it_1 != end_1 && it_2 != end_2)
+		{
+			if (! (*it_1 == *it_2))
+				return false;
+
+			++it_1;
+			++it_2;
+		}
+
+		// Ranges are equal iff they are of equal length and all elements compared equal.
+		return (it_1 == end_1 && it_2 == end_2);
+	}
+}
+#endif
+
+#endif
