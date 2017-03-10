5eb29d27383a72c485f74e65813a03a87cb53768
Tuukka Norri <tuukka.norri@hiit.fi>
2017-03-10 03:20:36 +0200

    Fix library paths

--- linux-static.local.mk
+++ linux-static.local.mk
@@ -7,4 +7,4 @@ GENGETOPT			= gengetopt
 
 BOOST_INCLUDE		= -I/home/tnorri/local/boost-1-60-0-gcc/include
 BOOST_LIBS			= -L/home/tnorri/local/boost-1-60-0-gcc/lib -lboost_iostreams
-LIBDISPATCH_LIBS	= ../../lib/libdispatch/libdispatch-build/src/libdispatch.a ../../lib/libpwq/libpwq-build/libpthread_workqueue.a /usr/lib/libkqueue.a -lpthread -static-libstdc++ -static-libgcc
+LIBDISPATCH_LIBS	= ../lib/libdispatch/libdispatch-build/src/libdispatch.a ../lib/libpwq/libpwq-build/libpthread_workqueue.a /usr/lib/libkqueue.a -lpthread -static-libstdc++ -static-libgcc
