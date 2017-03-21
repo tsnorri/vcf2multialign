86ef8134941510ed367b680816aad498dec566a9
tsnorri <tsnorri@iki.fi>
2017-03-21 16:48:18 +0200

    Bump the version number, append the Git version identifier to it

--- common.mk
+++ common.mk
@@ -6,7 +6,7 @@ OPT_FLAGS		= -O2 -g
 
 CFLAGS			= -std=c99   $(OPT_FLAGS) $(WARNING_FLAGS)
 CXXFLAGS		= -std=c++1z $(OPT_FLAGS) $(WARNING_FLAGS)
-CPPFLAGS		= -I../include -I../lib/libdispatch -I../lib/libpwq/include $(BOOST_INCLUDE)
+CPPFLAGS		= -DHAVE_CONFIG_H -I../include -I../lib/libdispatch -I../lib/libpwq/include $(BOOST_INCLUDE)
 LDFLAGS			= $(LIBDISPATCH_LIBS) $(BOOST_LIBS)
 
 
