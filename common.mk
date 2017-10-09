d679e88cd7d46a9f2f78a8826ee396b21c35d1ce
Tuukka Norri <tsnorri@iki.fi>
2017-10-09 15:39:01 +0300

    Link libemon.a

--- common.mk
+++ common.mk
@@ -7,7 +7,7 @@ OPT_FLAGS		= -O2 -g
 CFLAGS			= -std=c99   $(OPT_FLAGS) $(WARNING_FLAGS)
 CXXFLAGS		= -std=c++1z $(OPT_FLAGS) $(WARNING_FLAGS)
 CPPFLAGS		= -DHAVE_CONFIG_H -I../include -I../lib/lemon -I../lib/libdispatch -I../lib/libpwq/include $(BOOST_INCLUDE)
-LDFLAGS			= $(LIBDISPATCH_LIBS) $(BOOST_LIBS)
+LDFLAGS			= $(LIBDISPATCH_LIBS) $(BOOST_LIBS) ../lib/lemon/build/lemon/libemon.a
 
 
 %.o: %.cc
