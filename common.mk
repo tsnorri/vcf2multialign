7f0c123122bd1fd06e2103571c35265726011576
tsnorri <tsnorri@iki.fi>
2017-10-30 20:49:24 +0200

    Use -std=c++17 as a compiler flag instead of -std=c++1z

--- common.mk
+++ common.mk
@@ -5,10 +5,9 @@ WARNING_FLAGS	= -Wall -Werror -Wno-deprecated-declarations -Wno-unused
 OPT_FLAGS		= -O2 -g
 
 CFLAGS			= -std=c99   $(OPT_FLAGS) $(WARNING_FLAGS)
-CXXFLAGS		= -std=c++1z $(OPT_FLAGS) $(WARNING_FLAGS)
+CXXFLAGS		= -std=c++17 $(OPT_FLAGS) $(WARNING_FLAGS)
 CPPFLAGS		= -DHAVE_CONFIG_H -I../include -I../lib/lemon -I../lib/libdispatch -I../lib/libpwq/include $(BOOST_INCLUDE)
 LDFLAGS			= $(LIBDISPATCH_LIBS) $(BOOST_LIBS) ../lib/lemon/build/lemon/libemon.a
-
 
 %.o: %.cc
 	$(CXX) -c $(CXXFLAGS) $(CPPFLAGS) -o $@ $<
