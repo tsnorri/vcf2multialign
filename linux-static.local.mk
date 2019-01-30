8f801ef1b80cb242832436387902c2f78ae4f085
Tuukka Norri <tuukka.norri@hiit.fi>
2019-01-30 16:42:37 +0200

    Build with Clang on Linux
    – Apple’s libdispatch requires Clang anyway.

--- linux-static.local.mk
+++ linux-static.local.mk
@@ -1,9 +1,15 @@
 # Build a mostly static binary.
 
-CC					= gcc-7
-CXX					= g++-7
+LLVM_ROOT				= /usr/lib/llvm-6.0
+CLANG_INCLUDE_DIR		= $(LLVM_ROOT)/lib/clang/6.0.0/include
 
-BOOST_ROOT			= /home/tnorri/local/boost-1-64-0-g++-7.1
-BOOST_INCLUDE		= -I$(BOOST_ROOT)/include
-BOOST_LIBS			= -L$(BOOST_ROOT)/lib -lboost_iostreams
-LIBDISPATCH_LIBS	= ../lib/libdispatch/libdispatch-build/src/libdispatch.a ../lib/libpwq/libpwq-build/libpthread_workqueue.a /usr/lib/libkqueue.a -lpthread -static-libstdc++ -static-libgcc
+CC						= clang-6.0
+CXX						= clang++-6.0
+LIBDISPATCH_CFLAGS		= -U__STDC_HOSTED__ -isystem $(CLANG_INCLUDE_DIR)
+LIBDISPATCH_CXXFLAGS	= -U__STDC_HOSTED__ -isystem $(CLANG_INCLUDE_DIR)
+
+CFLAGS					= -fblocks -U__STDC_HOSTED__ -isystem $(CLANG_INCLUDE_DIR)
+CXXFLAGS				= -fblocks -U__STDC_HOSTED__ -isystem $(CLANG_INCLUDE_DIR)
+
+BOOST_LIBS				= -L$(BOOST_ROOT)/lib -lboost_iostreams
+LDFLAGS					= -static-libstdc++ -static-libgcc
