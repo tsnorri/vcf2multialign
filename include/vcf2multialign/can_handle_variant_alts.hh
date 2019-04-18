175a3d2b5c5b1c32d54502c236b30e93998507ed
tsnorri <tsnorri@iki.fi>
2019-04-18 03:15:24 +0300

    Remove vcf2multialign-specifics from libvcf2multialign

---
+++ include/vcf2multialign/can_handle_variant_alts.hh
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2017-2019 Tuukka Norri
+ * This code is licensed under MIT license (see LICENSE for details).
+ */
+
+#ifndef VCF2MULTIALIGN_CAN_HANDLE_VARIANT_ALTS_HH
+#define VCF2MULTIALIGN_CAN_HANDLE_VARIANT_ALTS_HH
+
+#include <libbio/vcf/variant.hh>
+
+
+namespace vcf2multialign {
+		
+	bool can_handle_variant_alts(libbio::transient_variant const &var);
+	bool can_handle_variant_alt(libbio::variant_alt_base const &alt);
+}
+
+#endif
---
+++ libvcf2multialign/can_handle_variant_alts.cc
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2017-2019 Tuukka Norri
+ * This code is licensed under MIT license (see LICENSE for details).
+ */
+
+#include <vcf2multialign/can_handle_variant_alts.hh>
+
+
+namespace lb	= libbio;
+namespace v2m	= vcf2multialign;
+
+
+namespace vcf2multialign {
+
+	bool can_handle_variant_alts(lb::transient_variant const &var)
+	{
+		// Keep.
+		for (auto const &alt : var.alts())
+		{
+			auto const svt(alt.alt_sv_type);
+			switch (svt)
+			{
+				// These structural variant types are currently handled.
+				case lb::sv_type::NONE:
+				case lb::sv_type::DEL:
+				case lb::sv_type::DEL_ME:
+					return true;
+					
+				case lb::sv_type::INS:
+				case lb::sv_type::DUP:
+				case lb::sv_type::INV:
+				case lb::sv_type::CNV:
+				case lb::sv_type::DUP_TANDEM:
+				case lb::sv_type::INS_ME:
+				case lb::sv_type::UNKNOWN:
+					break;
+			}
+		}
+		
+		return false;
+	}
+	
+	
+	bool can_handle_variant_alt(lb::variant_alt_base const &alt)
+	{
+		auto const svt(alt.alt_sv_type);
+		switch (svt)
+		{
+			case lb::sv_type::NONE:
+			case lb::sv_type::DEL:
+			case lb::sv_type::DEL_ME:
+				return true;
+				
+			case lb::sv_type::INS:
+			case lb::sv_type::DUP:
+			case lb::sv_type::INV:
+			case lb::sv_type::CNV:
+			case lb::sv_type::DUP_TANDEM:
+			case lb::sv_type::INS_ME:
+			case lb::sv_type::UNKNOWN:
+				return false;
+		}
+	}
+}
