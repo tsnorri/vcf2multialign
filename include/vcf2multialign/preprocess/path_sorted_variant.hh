facf371da972e39b2d026e9b629093d44a6c361c
tsnorri <tsnorri@iki.fi>
2019-08-19 21:49:12 +0300

    Add determining representative for each path

--- include/vcf2multialign/preprocess/path_sorted_variant.hh
+++ include/vcf2multialign/preprocess/path_sorted_variant.hh
@@ -18,17 +18,23 @@ namespace vcf2multialign {
 	protected:
 		path_vector			m_paths_by_sample;
 		sample_map			m_samples_by_path;
+		sample_vector		m_representatives_by_path;
 		
 	public:
 		void reserve_memory_for_paths(std::size_t path_count) { m_samples_by_path.resize(path_count); }
+		void reserve_memory_for_representatives(std::size_t path_count) { m_representatives_by_path.resize(path_count); }
 		
 		std::size_t path_count() const { return m_samples_by_path.size(); }
 		void set_paths_by_sample(path_vector const &paths) { m_paths_by_sample = paths; } // Copy.
 		path_vector const &paths_by_sample() const { return m_paths_by_sample; }
+		
 		sample_map const &samples_by_path() const { return m_samples_by_path; }
 		sample_map &samples_by_path() { return m_samples_by_path; }
+		sample_vector const &representatives_by_path() const { return m_representatives_by_path; }
+		sample_vector &representatives_by_path() { return m_representatives_by_path; }
 		
 		void invert_paths_by_sample();
+		void determine_representatives_for_each_sample();
 	};
 }
 
