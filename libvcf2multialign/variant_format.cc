4db0f9cf039f5d3442e5b864dd7aea903d9ea6d7
tsnorri <tsnorri@iki.fi>
2019-11-07 17:14:55 +0200

    Update libbio

--- libvcf2multialign/variant_format.cc
+++ libvcf2multialign/variant_format.cc
@@ -10,13 +10,6 @@
 namespace lb	= libbio;
 
 
-#define ASSIGN_FIELD_PTR(ID, DST) do { \
-	auto const it(this->m_fields_by_identifier.find(ID)); \
-	libbio_always_assert_neq(it, m_fields_by_identifier.end()); \
-	DST = dynamic_cast <decltype(DST)>(it->second.get()); \
-} while (false)
-
-
 namespace vcf2multialign {
 	
 	void variant_format::reader_will_update_format(lb::vcf_reader &reader)
@@ -26,6 +19,6 @@ namespace vcf2multialign {
 	
 	void variant_format::reader_did_update_format(lb::vcf_reader &reader)
 	{	
-		ASSIGN_FIELD_PTR("GT", gt);
+		this->assign_field_ptr("GT", gt);
 	}
 }
