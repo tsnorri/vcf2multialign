f161a31cb59f25312e8f131b974f91e9e25ca880
tsnorri <tsnorri@iki.fi>
2020-01-17 14:50:37 +0200

    Move find_overlapping_segment_range() to utility.hh

--- combine-msa-vcf/utility.hh
+++ combine-msa-vcf/utility.hh
@@ -34,6 +34,37 @@ namespace vcf2multialign {
 			)
 		);
 	}
+	
+	
+	// Find the segments that overlap with the given variant.
+	// The range from seg_it to seg_end needs to consist of consequtive segments sorted by their position.
+	template <typename t_iterator, typename t_variant>
+	inline auto find_overlapping_segment_range(
+		t_iterator const seg_it,
+		t_iterator const seg_end,
+		t_variant const &var
+	) -> std::pair <t_iterator, t_iterator>
+	{
+		struct {
+			bool operator()(t_variant const &var, aligned_segment const &seg) const
+			{
+				// lt. if the variant is located before this segment.
+				auto const var_pos(var.variant.zero_based_pos());
+				auto const var_end_pos(var_pos + var.size);
+				return var_end_pos <= seg.alt.position;
+			}
+			
+			bool operator()(aligned_segment const &seg, t_variant const &var) const
+			{
+				// lt. if the variant start is located after this segment.
+				auto const var_pos(var.variant.zero_based_pos());
+				auto const seg_end_pos(seg.alt_end());
+				return seg_end_pos <= var_pos;
+			}
+		} seg_cmp;
+		
+		return std::equal_range(seg_it, seg_end, var, seg_cmp);
+	}
 }
 
 #endif
