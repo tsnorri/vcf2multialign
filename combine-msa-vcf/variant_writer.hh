cb01171507e323c63ac1b2c542f6a2f4b082b138
tsnorri <tsnorri@iki.fi>
2020-01-17 17:40:59 +0200

    Add check for overlap count gt. ploidy

--- combine-msa-vcf/variant_writer.hh
+++ combine-msa-vcf/variant_writer.hh
@@ -23,6 +23,10 @@ namespace vcf2multialign {
 	
 	struct variant_description
 	{
+	protected:
+		struct private_tag {};
+		
+	public:
 		// The genotype is stored in a std::vector <bool> here.
 		// A pair of integers could be used instead, though, since
 		// at the moment we only handle unphased VCF files and thus
@@ -36,6 +40,31 @@ namespace vcf2multialign {
 		variant_origin		origin{};
 		bool				is_skipped{};
 		
+	protected:
+		template <typename t_string_1, typename t_string_2>
+		variant_description(
+			std::size_t const position_,
+			t_string_1 &&ref_,
+			t_string_2 &&alt_,
+			std::size_t const ploidy,
+			std::int32_t const overlap_count_,
+			variant_origin const origin_,
+			private_tag const
+		):
+			ref(std::forward <t_string_1>(ref_)),
+			alt(std::forward <t_string_2>(alt_)),
+			genotype(ploidy, false),
+			position(position_),
+			overlap_count(overlap_count_),
+			origin(origin_)
+		{
+			libbio_assert_lte(overlap_count, ploidy);
+			// Assign the GT values.
+			genotype.resize(overlap_count);
+			genotype.resize(ploidy, true);
+		}
+		
+	public:
 		variant_description() = default;
 		
 		variant_description(std::size_t const ploidy, std::int32_t const gt_count, variant_origin const origin_):
@@ -57,17 +86,16 @@ namespace vcf2multialign {
 			std::int32_t const overlap_count_,
 			variant_origin const origin_
 		):
-			ref(std::forward <t_string_1>(ref_)),
-			alt(std::forward <t_string_2>(alt_)),
-			genotype(ploidy, false),
-			position(position_),
-			overlap_count(overlap_count_),
-			origin(origin_)
+			variant_description(
+				position_,
+				std::forward <t_string_1>(ref_),
+				std::forward <t_string_2>(alt_),
+				ploidy,
+				(overlap_count_ < 0 ? overlap_count_ : std::min <std::int32_t>(overlap_count_, ploidy)),
+				origin_,
+				private_tag()
+			)
 		{
-			libbio_assert_lte(overlap_count, ploidy);
-			// Assign the GT values.
-			genotype.resize(overlap_count);
-			genotype.resize(ploidy, true);
 		}
 	};
 	
