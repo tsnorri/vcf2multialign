2f7c5acf0cd8b5603acff9f844423c7248881902
Tuukka Norri <tuukka.norri@hiit.fi>
2020-02-20 16:05:34 +0200

    Add tool for checking whether variants in one file are listed in another

---
+++ vcfcontains/vcf_record_generator.cc
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020 Tuukka Norri
+ * This code is licensed under MIT license (see LICENSE for details).
+ */
+
+#include <libbio/vcf/variant.hh>
+#include "vcf_record_generator.hh"
+
+
+namespace lb = libbio;
+
+
+namespace vcf2multialign {
+
+	void vcf_record_generator::prepare()
+	{
+		this->prepare_reader();
+		
+		auto &reader(this->m_vcf_reader);
+		reader.reset();
+	}
+	
+	
+	bool vcf_record_generator::next_variant(lb::variant &out_var)
+	{
+		bool retval(false);
+		bool const st(this->m_vcf_reader.parse_one([this, &retval, &out_var](lb::transient_variant const &var) {
+			// Not reached on EOF.
+			out_var = var;
+			retval = true;
+			return true;
+		}, m_parser_state));
+	
+		return retval;
+	}
+}
