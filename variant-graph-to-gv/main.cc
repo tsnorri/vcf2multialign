99e66d7f71fb35b47baf671b463e8b75812dde1d
Tuukka Norri <tuukka.norri@hiit.fi>
2020-08-20 11:59:34 +0300

    Add option to output given subgraph only

--- variant-graph-to-gv/main.cc
+++ variant-graph-to-gv/main.cc
@@ -9,6 +9,7 @@
 #include <libbio/assert.hh>
 #include <libbio/dispatch.hh>
 #include <range/v3/view/sliding.hpp>
+#include <range/v3/view/zip.hpp>
 #include <unistd.h>
 #include <vcf2multialign/graph/variant_graph.hh>
 #include <vcf2multialign/utility/read_single_fasta_seq.hh>
@@ -16,6 +17,7 @@
 
 
 namespace lb	= libbio;
+namespace rsv	= ranges::view;
 namespace v2m	= vcf2multialign;
 
 
@@ -25,7 +27,8 @@ namespace {
 		char const *input_graph_path,
 		char const *output_graph_path,
 		char const *reference_seq_name,
-		std::size_t const max_aligned_pos,
+		std::size_t const subgraph_idx,
+		std::size_t max_aligned_pos,
 		bool const should_overwrite_files
 	)
 	{
@@ -65,6 +68,25 @@ namespace {
 		auto const &alt_edge_count_csum(graph.alt_edge_count_csum());
 		auto const &alt_edge_targets(graph.alt_edge_targets());
 		auto const &alt_edge_labels(graph.alt_edge_labels());
+
+		if (SIZE_MAX != subgraph_idx && subgraph_start_positions.size() <= subgraph_idx)
+		{
+			std::cerr << "Got subgraph index " << subgraph_idx << " while there are " << subgraph_start_positions.size() << " subgraphs.\n";
+			std::exit(EXIT_FAILURE);
+		}
+
+		// Check whether iteration should be started from a subgraph.
+		// Convert the indices to 1-based.
+		auto const pos_slice_start(1 + (SIZE_MAX == subgraph_idx ? 0 : subgraph_start_positions[subgraph_idx]));
+		auto const pos_slice_end(
+			SIZE_MAX == subgraph_idx || subgraph_start_positions.size() <= (1 + subgraph_idx)
+			? ref_positions.size()
+			: 2 + subgraph_start_positions[1 + subgraph_idx]
+		);
+
+		// Update max_aligned_pos to point to the subgraph end.
+		max_aligned_pos = std::min(max_aligned_pos, aligned_ref_positions[pos_slice_end - 1]);
+
 		output_graph_stream << "digraph variants {\n";
 		output_graph_stream << "\trankdir = LR;\n";
 		output_graph_stream << "\trank = same;\n";
@@ -72,11 +94,18 @@ namespace {
 		// Labels if needed, otherwise use node ID as the label.
 		{
 			// Since background colours are needed at subgraph start positions, handle them, too.
-			auto sg_it(subgraph_start_positions.begin());
+			auto sg_it(subgraph_start_positions.begin() + (SIZE_MAX == subgraph_idx ? 0 : subgraph_idx));
 			auto const sg_end(subgraph_start_positions.end());
 			std::size_t next_subgraph_start_idx(sg_it == sg_end ? SIZE_MAX : *sg_it++);
-			auto const rsv(ranges::view::zip(ranges::view::ints(0), ref_positions | ranges::view::tail, aligned_ref_positions | ranges::view::tail));
-			for (auto const &[i, ref_pos, aligned_ref_pos] : rsv)
+
+			auto const range(
+				rsv::zip(
+					rsv::ints(0),
+					ref_positions | rsv::slice(pos_slice_start, pos_slice_end),
+					aligned_ref_positions | rsv::slice(pos_slice_start, pos_slice_end)
+				)
+			);
+			for (auto const &[i, ref_pos, aligned_ref_pos] : range)
 			{
 				if (max_aligned_pos < aligned_ref_pos)
 					break;
@@ -96,11 +125,11 @@ namespace {
 
 		// REF edges.
 		{
-			auto const rsv(ranges::view::zip(
-				ref_positions | ranges::view::tail | ranges::view::sliding(2),
-				aligned_ref_positions | ranges::view::tail | ranges::view::sliding(2)
+			auto const range(rsv::zip(
+				ref_positions | rsv::slice(pos_slice_start, pos_slice_end) | rsv::sliding(2),
+				aligned_ref_positions | rsv::slice(pos_slice_start, pos_slice_end) | rsv::sliding(2)
 			));
-			for (auto const &[ref_pair, aligned_ref_pair] : rsv)
+			for (auto const &[ref_pair, aligned_ref_pair] : range)
 			{
 				auto const aligned_lhs(aligned_ref_pair[0]);
 				auto const aligned_rhs(aligned_ref_pair[1]);
@@ -123,6 +152,7 @@ namespace {
 					output_graph_stream << ref_sub.substr(0, 10);
 					output_graph_stream << "…";
 					output_graph_stream << ref_sub.substr(ref_sub.size() - 10, 10);
+					output_graph_stream << " (" << ref_sub.size() << ')';
 				}
 				else
 				{
@@ -135,12 +165,12 @@ namespace {
 		
 		// ALT edges.
 		{
-			std::size_t i(0);
-			for (auto const &pair : alt_edge_count_csum | ranges::view::sliding(2))
+			std::size_t i(pos_slice_start);
+			for (auto const &pair : alt_edge_count_csum | rsv::slice(pos_slice_start, pos_slice_end) | rsv::sliding(2))
 			{
 				auto const start(pair[0]);
 				auto const end(pair[1]);
-				for (auto const &[target, label] : ranges::view::slice(ranges::view::zip(alt_edge_targets, alt_edge_labels), start, end))
+				for (auto const &[target, label] : rsv::slice(rsv::zip(alt_edge_targets, alt_edge_labels), start, end))
 				{
 					auto const src(aligned_ref_positions[1 + i]);
 					auto const dst(aligned_ref_positions[1 + target]);
@@ -175,6 +205,17 @@ int main(int argc, char **argv)
 	std::ios_base::sync_with_stdio(false);	// Don't use C style IO after calling cmdline_parser.
 	std::cin.tie(nullptr);					// We don't require any input from the user.
 
+	std::size_t subgraph_idx(SIZE_MAX);
+	if (args_info.subgraph_given)
+	{
+		if (args_info.subgraph_arg < 0)
+		{
+			std::cerr << "Subgraph index needs to be non-negative.\n";
+			std::exit(EXIT_FAILURE);
+		}
+		subgraph_idx = args_info.subgraph_arg;
+	}
+
 	std::size_t max_aligned_pos(SIZE_MAX);
 	if (args_info.max_aligned_pos_given)
 	{
@@ -193,6 +234,7 @@ int main(int argc, char **argv)
 			args_info.variants_arg,
 			args_info.output_arg,
 			args_info.reference_sequence_given ? args_info.reference_sequence_arg : nullptr,
+			subgraph_idx,
 			max_aligned_pos,
 			args_info.overwrite_flag
 		);
