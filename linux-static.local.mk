a94543156eeaad0f56c0bd6d8530c545ed74d843
Tuukka Norri <tsnorri@iki.fi>
2023-08-29 13:45:35 +0300

    Remove unneeded variables

--- linux-static.local.mk
+++ linux-static.local.mk
@@ -5,8 +5,6 @@ CLANG_INCLUDE_DIR		= $(LLVM_ROOT)/lib/clang/15/include
 
 CC							= clang-15
 CXX							= clang++-15
-LIBDISPATCH_CFLAGS			= -U__STDC_HOSTED__ -isystem $(CLANG_INCLUDE_DIR) 
-LIBDISPATCH_CXXFLAGS		= -U__STDC_HOSTED__ -isystem $(CLANG_INCLUDE_DIR) -stdlib=libc++
 
 # Make Boost not use std::unary_function and std::binary_function with BOOST_NO_CXX98_FUNCTION_BASE. (These have been deprecated.)
 # Boost (as of 1.80.0) correctly defines the macro for libstdc++ but for some reason does not for libc++.
