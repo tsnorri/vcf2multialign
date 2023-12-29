da076bb6f88ea1c334eedec4ca1aba3a4bf5b8e0
Tuukka Norri <tsnorri@iki.fi>
2023-12-29 20:38:23 +0200

    Begin fixing compiler, linker flags

--- conda/build.sh
+++ conda/build.sh
@@ -12,15 +12,11 @@ fi
 # FIXME: hack to make conda-build use Clang on Linux. This should be made platform-independent.
 clang="${PREFIX}/bin/clang"
 clangxx="${PREFIX}/bin/clang++"
-#cxx_includes="-isystem '${PREFIX}/include/c++/v1'"
 clang_version=`${PREFIX}/bin/clang -dumpversion`
-includes="-I '${SRC_DIR}/conda'"
-cxx_includes="-isystem '${PREFIX}/x86_64-conda-linux-gnu/include/c++/9.3.0' -isystem '${PREFIX}/x86_64-conda-linux-gnu/include/c++/9.3.0/x86_64-conda-linux-gnu'"
-sys_includes="-isystem '${PREFIX}/lib/clang/${clang_version}/include' -isystem '${PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/include'"
-cppflags="-nostdinc -U__STDC_HOSTED__ ${includes}"
-system_cppflags="${cxx_includes} ${sys_includes}"
-cflags="-fblocks"
-cxxflags="-fblocks -nostdinc++"
+includes="--sysroot '${PREFIX}/x86_64-conda-linux-gnu/sysroot' -isystem =/../../include/c++/13.2.0 -isystem =/../include -isystem =/../../include" # -I'${SRC_DIR}/conda'"
+cppflags="${includes}"
+cflags="-fblocks -Wno-unknown-warning-option"
+cxxflags="-fblocks -Wno-unknown-warning-option"
 
 echo "CC = ${clang}"																> local.mk
 echo "CXX = ${clangxx}"																>> local.mk
@@ -28,14 +24,17 @@ echo "CPPFLAGS = ${cppflags}"														>> local.mk
 echo "CFLAGS = ${cflags}"															>> local.mk
 echo "CXXFLAGS = ${cxxflags}"														>> local.mk
 echo "LDFLAGS = -L${PREFIX}/lib -ldl"												>> local.mk
-echo "SYSTEM_CPPFLAGS = ${system_cppflags}"											>> local.mk
-echo "LIBDISPATCH_CFLAGS = ${cppflags} ${sys_includes}"								>> local.mk
-echo "LIBDISPATCH_CXXFLAGS = ${cppflags} -nostdinc ${cxx_includes} ${sys_includes}"	>> local.mk
+echo "SYSTEM_CPPFLAGS = ${cppflags}"												>> local.mk
+#echo "LIBDISPATCH_CFLAGS = ${cppflags}"												>> local.mk
+#echo "LIBDISPATCH_CXXFLAGS = ${cppflags}"											>> local.mk
 echo "BOOST_ROOT = ${PREFIX}"														>> local.mk
+echo "BOOST_INCLUDE ="																>> local.mk
 echo "BOOST_LIBS = -lboost_iostreams"												>> local.mk
 echo "LIBBSD_LIB = ${PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/lib64/libbsd.a"		>> local.mk
 
-make -j ${CPU_COUNT}
+cat local.mk
+
+make #-j ${CPU_COUNT}
 
 dst_bin="${PREFIX}/bin"
 mkdir -p "${dst_bin}"
